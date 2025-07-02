f32d5cbb73df995fce5496a7d0bb6af4e7321615
MariiaRev <maria.revenko2209@gmail.com>
2025-07-02 17:05:53 +0300

    added service for checking if user exists or is admin

---
+++ URLShortener/URLShortener/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> IsAdminAsync(string userId);
+        Task<bool> UserExistsAsync(string userId);
+    }
+}
---
+++ URLShortener/URLShortener/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using URLShortener.Services.Interfaces;
+
+namespace URLShortener.Services
+{
+    public class UserService(UserManager<IdentityUser> userManager) : IUserService
+    {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+
+        public async Task<bool> IsAdminAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return false;
+
+            return await _userManager.IsInRoleAsync(user, "Admin");
+        }
+
+        public async Task<bool> UserExistsAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return user != null;
+        }
+    }
+}
